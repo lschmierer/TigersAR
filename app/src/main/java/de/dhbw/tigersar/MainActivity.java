b77074f7371c96c95a2a2bd2981b7c9db30732e8
Lukas Schmierer <lukas.schmierer@lschmierer.de>
2016-11-10 00:10:29 +0100

    calculate and set aspect ratio

--- app/src/main/java/de/dhbw/tigersar/MainActivity.java
+++ app/src/main/java/de/dhbw/tigersar/MainActivity.java
@@ -1,6 +1,9 @@
 package de.dhbw.tigersar;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import org.artoolkit.ar.base.ARActivity;
@@ -8,10 +11,20 @@ import org.artoolkit.ar.base.rendering.ARRenderer;
 
 public class MainActivity extends ARActivity {
 
+    private static final float MAIN_LAYOUT_ASPECT_RATIO = 4 / 3f;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainLayout = supplyFrameLayout();
+
+        Point mainLayoutSize = calculateMainLayoutSize();
+        ViewGroup.LayoutParams layoutParams = mainLayout.getLayoutParams();
+        layoutParams.width = mainLayoutSize.x;
+        layoutParams.height = mainLayoutSize.y;
+        mainLayout.setLayoutParams(layoutParams);
     }
 
     @Override
@@ -22,6 +35,20 @@ public class MainActivity extends ARActivity {
     @Override
     protected FrameLayout supplyFrameLayout() {
         return (FrameLayout) findViewById(R.id.aRFramleLayout);
+    }
+
+    private Point calculateMainLayoutSize() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point displaySize = new Point();
+        display.getSize(displaySize);
+        int width = displaySize.x;
+        int height = displaySize.y;
+
+        if (height * MAIN_LAYOUT_ASPECT_RATIO < width) {
+            return new Point(width, Math.round(width / MAIN_LAYOUT_ASPECT_RATIO));
+        } else {
+            return new Point(Math.round(MAIN_LAYOUT_ASPECT_RATIO * height), height);
+        }
     }
 
 }

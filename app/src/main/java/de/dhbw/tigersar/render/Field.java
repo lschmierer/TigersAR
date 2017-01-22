29cf445c77782086d0594631a338176beb2a8e29
Lukas Schmierer <lukas.schmierer@lschmierer.de>
2017-01-22 13:08:16 +0100

    adjust field line width

--- app/src/main/java/de/dhbw/tigersar/render/Field.java
+++ app/src/main/java/de/dhbw/tigersar/render/Field.java
@@ -16,15 +16,15 @@ public class Field implements Renderable {
     public Field(int width, int height) {
         lines = new ArrayList<>(5);
         // oben
-        lines.add(new Line(200, new float[]{-width / 2, height / 2, 0}, new float[]{width / 2, height / 2, 0}));
+        lines.add(new Line(8, new float[]{-width / 2, height / 2, 0}, new float[]{width / 2, height / 2, 0}));
         // unten
-        lines.add(new Line(200, new float[]{-width / 2, -height / 2, 0}, new float[]{width / 2, -height / 2, 0}));
+        lines.add(new Line(8, new float[]{-width / 2, -height / 2, 0}, new float[]{width / 2, -height / 2, 0}));
         // mitte
-        lines.add(new Line(200, new float[]{0, -height / 2, 0}, new float[]{0, height / 2, 0}));
+        lines.add(new Line(8, new float[]{0, -height / 2, 0}, new float[]{0, height / 2, 0}));
         // links
-        lines.add(new Line(200, new float[]{-width / 2, height / 2, 0}, new float[]{-width / 2, -height / 2, 0}));
+        lines.add(new Line(8, new float[]{-width / 2, height / 2, 0}, new float[]{-width / 2, -height / 2, 0}));
         // rechts
-        lines.add(new Line(200, new float[]{width / 2, height / 2, 0}, new float[]{width / 2, -height / 2, 0}));
+        lines.add(new Line(8, new float[]{width / 2, height / 2, 0}, new float[]{width / 2, -height / 2, 0}));
 
         for (Line line : lines) {
             line.setColor(new float[]{1f, 1f, 1f, 1f});
